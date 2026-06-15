1bfeedb3cebdc83a9b3bbb563e4942d6f3acf23d
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-06-15 10:47:14 +0300

    add bootstrap in Login (form)

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,44 +1,40 @@
- import React from 'react';
- import { useFormik } from 'formik';
-import { Form, Button, Container } from 'react-bootstrap';
+import React from 'react';
+import { useFormik } from 'formik';
+import { Form, Button, Container, Card, FloatingLabel } from 'react-bootstrap';
 
 const Login = () => {
-     const formik = useFormik({
-     initialValues: {
-       name: '',
-       password: ''
-     },
-     onSubmit: values => {
-       alert(JSON.stringify(values, null, 2));
-     },
-   });
-   return (
-     <Form onSubmit={formik.handleSubmit}>
-         <Container class="form-floating mb-3">
-       <Form.Label htmlFor="name">Ваш ник</Form.Label>
-       <Form.Control
-         id="name"
-         name="name"
-         type="text"
-         placeholder='Ваш ник'
-        //  onChange={formik.handleChange}
-        //  value={formik.values.email}
-       />
-       </Container>
-        <Container class="form-floating mb-3">
-           <Form.Label htmlFor="password">Пароль</Form.Label>
-       <Form.Control
-         id="password"
-         name="password"
-         type="password"
-              placeholder='Пароль'
-         onChange={formik.handleChange}
-         value={formik.values.email}
-       />
- </Container>
-       <Button type="submit">Войти</Button>
-     </Form>
-   );
- };
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      password: ''
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <Card>
+      <Card.Header>Войти</Card.Header>
+      <Card.Body>
+        <Form onSubmit={formik.handleSubmit}>
+          <FloatingLabel controlId='name' label='Ваш ник' className='mb-3'>
+            <Form.Control id="name" name="name" type="text"placeholder='Ваш ник'
+              //  onChange={formik.handleChange}
+              //  value={formik.values.email}
+            />
+          </FloatingLabel>
+          <FloatingLabel controlId='password' label='Пароль' className='mb-3'>
+            <Form.Control id="password" name="password" type="password" placeholder='Пароль'
+              // onChange={formik.handleChange}
+              // value={formik.values.email}
+            />
+            <Form.Label htmlFor="password">Пароль</Form.Label>
+          </FloatingLabel>
+          <Button type="submit">Войти</Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default Login
