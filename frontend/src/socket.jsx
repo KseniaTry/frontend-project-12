4b092e28a049c7a2a876bea332646b4716839c89
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-06-21 16:33:59 +0300

    add correct socket connection

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,9 +1,4 @@
 import { io } from 'socket.io-client';
 
-// Инициализация соединения
-export const socket = io('<http://localhost:5002');
-
-// const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:5002';
-
-// export const socket = io(URL);
+export const socket = io();
 
