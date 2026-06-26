3417fa30c6eadf2135ffbc9c25978579d6309e6f
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-06-26 22:23:42 +0300

    make one channelModal for rename and add channel

--- frontend/src/components/ChannelModal.jsx
+++ frontend/src/components/ChannelModal.jsx
@@ -2,17 +2,25 @@
 import { Modal, Button, Form } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { useFormik } from "formik";
-import { addChannel, selectAllChannels } from "../slices/channelsSlice";
+import { addChannel, selectAllChannels, editChannel, selectChannelById } from "../slices/channelsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import * as yup from 'yup';
 
-const ChannelModal = ({show, onHide}) => {
+const ChannelModal = ({ show, onHide, type}) => {
   const {t} = useTranslation()
   const dispatch = useDispatch()
   const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const channels = useSelector(selectAllChannels)
+  const activeChannelId = localStorage.getItem('activeChannel')
+  const activeChannel = useSelector((state) => {
+    if (activeChannelId)  {
+      return  selectChannelById(state, activeChannelId)
+    } else {
+      return null
+    }
+  })
 
   const schema = yup.object().shape({
     channelName: yup.string()
@@ -30,9 +38,10 @@ const ChannelModal = ({show, onHide}) => {
 
   const formik = useFormik({
     initialValues: {
-      channelName: '',
+      channelName: type === 'rename' ? activeChannel?.name : '',
     },
     validationSchema: schema,
+    enableReinitialize: true, // нужно для того чтобы при переименовании отобразилось имя текущего канала (тк данные приходят не сразу)
     onSubmit: async (values) => {
       setError('')
              
@@ -41,9 +50,21 @@ const ChannelModal = ({show, onHide}) => {
       }
 
       try {
-        await dispatch(addChannel(newChannel))
+        switch (type) {
+          case 'add':
+            await dispatch(addChannel(newChannel))
+            break
+          case 'rename':
+            await dispatch(editChannel({channelId: activeChannelId, editedChannel: newChannel}))
+            break
+          default: 
+            console.log('type not exist')
+            break
+        }
+   
         setIsLoading(true)
         formik.resetForm()
+        onHide()
       } catch(err) {
         setIsLoading(false)
         setError(`Ошибка сервера: ${err.message}. Перезагруите страницу`);
@@ -63,7 +84,7 @@ const ChannelModal = ({show, onHide}) => {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          {t('channelModal.title')}
+          {type === 'add' ? t('channelModal.addTitle') : t('channelModal.renameTitle')}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
