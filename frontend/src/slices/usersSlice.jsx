7a352727e7b51d277b245b0cd8b8268595d29bec
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-06-29 10:49:49 +0300

    add usersSlice

---
+++ frontend/src/slices/usersSlice.jsx
@@ -0,0 +1,60 @@
+import { createSlice, createEntityAdapter, createAsyncThunk , current} from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const usersAdapter = createEntityAdapter()
+
+export const createNewUser = createAsyncThunk(
+  'users/createNewUser', 
+  async ({username, password}, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        '/api/v1/signup', 
+        { username,  password })
+      return response.data  // => { token: ..., username: 'newuser' }
+    } catch(err) {
+      // с помощью thunkAPI мы получаем конкретный код ошибки (401), 
+      // чтобы далее можно было корректно ее обработать и сделать 
+      // перенаправление на страницу Login при отсутствии токена авторизации
+      return thunkAPI.rejectWithValue({ status: err.response?.status, data: err.response?.data }) 
+    }
+  })
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: usersAdapter.getInitialState({
+    loadingStatus: false,
+    error: null,
+  }),
+  reducers: {
+    addNewUser: (state, action) => {
+      usersAdapter.addOne(state, action.payload)
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+    // получение всех каналов
+      .addCase(createNewUser.pending, (state) => {
+        state.loadingStatus = 'loading'
+      })
+      .addCase(createNewUser.fulfilled, (state, action) => { //  action.payload = response.data
+        usersAdapter.addOne(state, action.payload)
+        console.log(action.payload)
+        state.loadingStatus = 'idle'
+      })
+      .addCase(createNewUser.rejected, (state, action) => {
+        state.loadingStatus = 'failed'
+        state.error = action.payload.status === 409 ? 'Пользователь уже существует' : `Ошибка сервера: ${action.payload.error}. Перезагруите страницу` 
+      })
+  }
+})
+
+const baseSelectors = usersAdapter.getSelectors((state) => state.users);
+
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+  // selectIds: selectChannelIds        // Возвращает массив только с ID каналов
+} = baseSelectors;
+
+export const { addNewUser } = usersSlice.actions
+export default usersSlice.reducer
