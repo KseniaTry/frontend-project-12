36955dfdc27fd7c9920b4b37fd8114ea9cb0b049
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-07-08 14:47:50 +0300

    change host for 5002

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: 'http://localhost:5002',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
